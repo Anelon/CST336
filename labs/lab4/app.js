c7a535d140a62b32e49c4c40910cb6bc37afac6e
Andrew Bell <majacow@gmail.com>
2019-11-14 19:25:04 -0800

    lab 4 done?

--- labs/lab4/app.js
+++ labs/lab4/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.engine("html", require('ejs').renderFile);
+app.set('port',  process.env.PORT || "8080");
+app.set('ip',  process.env.IP || "0.0.0.0");
 app.use(express.static("public"));
 
 //routes
@@ -8,18 +10,22 @@ app.get("/", function(req, res) {
     res.render("index.ejs");
 });
 app.get("/mercury", function(req, res) {
-    res.send("Future home of Mercury");
+    res.render("mercury.ejs");
 });
 app.get("/venus", function(req, res) {
-    res.send("Future home of Venus");
+    res.render("venus.ejs");
 });
 app.get("/earth", function(req, res) {
-    res.send("Future home of Earth");
+    res.render("earth.ejs");
 });
 
+app.listen(app.get('port'), app.get('ip'),()=>{console.log("Express Server is Running...");});
+
+/*
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Express server is running...");
 });
+*/
 /*
     //local
 app.listen("8080", "127.0.0.1", function() {
